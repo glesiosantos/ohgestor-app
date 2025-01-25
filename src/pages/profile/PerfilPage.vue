d905f03590c41309035309741b7154f80d23fb2b
Glêsio Santos <glesioss@gmail.com>
2025-01-25 12:00:50 -0300

    adicionando header no dash

--- src/pages/profile/PerfilPage.vue
+++ src/pages/profile/PerfilPage.vue
@@ -1,9 +1,29 @@
 <template>
   <q-page padding>
-    <h1 class="text-h5">Perfil</h1>
+    <q-list bordered class="rounded-borders" style="max-width: 350px">
+      <q-item-label header>{{store.auth.perfil}}</q-item-label>
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">{{store.auth.apelido}}</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">E-mail</span>
+
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script setup>
-//
+import { useAuthStore } from 'src/stores/auth_store';
+
+const store = useAuthStore()
 </script>
