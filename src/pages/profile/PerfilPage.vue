4d9703747528834451b645bc16b795d45e5ff6da
Glêsio Santos <glesioss@gmail.com>
2025-02-22 13:00:55 -0300

    editando o perfil de usuário

--- src/pages/profile/PerfilPage.vue
+++ src/pages/profile/PerfilPage.vue
@@ -1,24 +1,67 @@
 <template>
   <q-page padding>
-    <q-list bordered class="rounded-borders" style="max-width: 350px">
-      <q-item-label header>{{store.auth.perfil}}</q-item-label>
+    <q-card class="q-pa-md">
+      <q-card-section>
+        <div class="text-h6">Meu Perfil</div>
+      </q-card-section>
 
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-          </q-avatar>
-        </q-item-section>
+      <q-card-section>
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Nome</q-item-label>
+              <q-item-label>{{ store.auth.apelido }}</q-item-label>
+            </q-item-section>
+          </q-item>
 
-        <q-item-section>
-          <q-item-label lines="1">{{store.auth.apelido}}</q-item-label>
-          <q-item-label caption lines="2">
-            <span class="text-weight-bold">E-mail</span>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>E-mail</q-item-label>
+              <q-item-label></q-item-label>
+            </q-item-section>
+          </q-item>
 
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Perfil</q-item-label>
+              <q-item-label>{{ store.auth.perfil }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+
+      <q-card-section>
+        <span>Alterar a senha de acesso</span>
+        <q-form @submit="changePassword">
+          <div class="row q-col-gutter-sm q-mt-xs">
+            <q-input
+            v-model="newPassword"
+            label="Nova Senha"
+            type="password"
+            class="col-12 col-md-6"
+            outlined
+            lazy-rules
+            :rules="[val => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']"
+          />
+
+          <q-input
+            v-model="newPassword"
+            label="Confirma senha"
+            type="password"
+            class="col-12 col-md-6"
+            outlined
+            lazy-rules
+            :rules="[val => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']"
+          />
+          </div>
+
+          <div class="row q-gutter-sm">
+            <q-btn label="Alterar Senha" type="submit" color="primary" :class="{'full-width': $q.screen.xs}"/>
+            <q-btn label="Cancelar" @click="handleCancelar" size="md" color="red" :class="{'full-width': $q.screen.xs}"/>
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
