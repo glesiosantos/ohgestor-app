b10c9c72607499135b9989e792205fc47c6b3b15
Glêsio Santos <glesioss@gmail.com>
2025-02-23 14:27:07 -0300

    finalizando recuperação de acesso

--- src/pages/auth/ForgoutPage.vue
+++ src/pages/auth/ForgoutPage.vue
@@ -5,7 +5,8 @@
       <q-separator class="full-width q-ma-lg"/>
       <q-form @submit.prevent="handleForm" class="q-gutter-y-sm">
         <q-input
-          outlined v-model="form.email"
+          outlined
+          v-model="form.email"
           type="email"
           label="E-mail"
           lazy-rules
@@ -25,17 +26,29 @@
 </template>
 
 <script setup>
+import useNotify from 'src/composables/UseNotify';
+import { authService } from 'src/services/auth_service';
 import { reactive } from 'vue'
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router'
 
-const router = useRouter()
-const form = reactive({
+const { notfifyError, notfifySucess} = useNotify()
+
+const { recuperarAcesso } = authService()
+// const router = useRouter()
+let form = reactive({
   email: ''
 })
 
-const handleForm = () => {
-  console.log(form)
-  router.push({ name: 'dashboard' })
+const handleForm = async () => {
+  await recuperarAcesso(form.email).then(response => {
+      console.log(response)
+      notfifySucess("Enviamos um e-mail para você informando a sua nova credencial")
+    }).catch(error => {
+      console.log(error)
+      notfifyError(error.response.data.mensagem)
+    })
+  form.email = ''
+   // router.push({ name: 'sign-in' })
 }
 
 </script>
